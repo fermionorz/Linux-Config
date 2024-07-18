e4bf6da32c014203a5c25ca727f61c3ea9841789
Zer0-hex <112745175+Zer0-hex@users.noreply.github.com>
2024-07-18 09:25:30 +0800

    Update kali.sh
    change kali.sh to add sudo

--- kali.sh
+++ kali.sh
@@ -7,10 +7,10 @@ sudo apt-get install -y xinit psmisc unzip neovim curl alacritty arandr flamesho
 sudo apt-get install -y libxcb-shape0-dev libxcb-keysyms1-dev libpango1.0-dev libxcb-util0-dev libxcb1-dev libxcb-icccm4-dev libyajl-dev libev-dev libxcb-xkb-dev libxcb-cursor-dev libxkbcommon-dev libxcb-xinerama0-dev libxkbcommon-x11-dev libstartup-notification0-dev libxcb-randr0-dev libxcb-xrm0 libxcb-xrm-dev autoconf libxcb-render-util0-dev libxcb-shape0-dev libxcb-xfixes0-dev
 
 # Font Install 字体安装
-mkdir -p ~/.local/share/fonts/
+sudo mkdir -p ~/.local/share/fonts/
 wget https://github.com/ryanoasis/nerd-fonts/releases/download/latest/Iosevka.zip
 wget https://github.com/ryanoasis/nerd-fonts/releases/download/latest/RobotoMono.zip
-unzip Iosevka.zip -d ~/.local/share/fonts/
-unzip RobotoMono.zip -d ~/.local/share/fonts/
-fc-cache -fv
+sudo unzip Iosevka.zip -d ~/.local/share/fonts/
+sudo unzip RobotoMono.zip -d ~/.local/share/fonts/
+sudo fc-cache -fv
 rm Iosevka.zip RobotoMono.zip
